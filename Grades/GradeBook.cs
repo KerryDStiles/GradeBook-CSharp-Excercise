23de80cd4c6ea9f7b79b36e203333d5386f72d23
Kerry D Stiles <KerryDStiles@gmail.com>
2016-09-29 16:45:31 -0700

    Add files via upload

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeBook : GradeTracker
+    {
+        public override IEnumerator GetEnumerator()
+        {
+            return grades.GetEnumerator();
+        }
+        protected List<float> grades;
+
+
+        public override void WriteGrades(TextWriter destination)
+        {
+            for (int i = 0; i < grades.Count; i++)
+            {
+                destination.WriteLine(grades[i]);
+            }
+        }
+        public GradeBook()//Constructor
+        {
+
+            grades = new List<float>();
+        }
+        public override void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public override GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+    }
+}
---
+++ Grades/GradeStatistics.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeStatistics
+    {
+        public GradeStatistics()
+        {
+            HighestGrade = 0;
+            LowestGrade = float.MaxValue;
+
+        }
+
+        public string Description
+        {
+            get
+            {
+                string result;
+                switch (LetterGrade)
+                {
+                    case "A":
+                        result = "Great Job!";
+                        break;
+                    case "B":
+                        result = "Well Done";
+                        break;
+                    case "C":
+                        result = "Keep Studying";
+                        break;
+                    case "D":
+                        result = "Try Harder";
+                        break;
+                    default:
+                        result = "See me after class!";
+                        break;
+                }
+                return result;
+            }
+
+        }
+        public string LetterGrade
+        {
+            get
+            {
+                string result;
+                if(AverageGrade >= 90)
+                {
+                    result = "A";
+                }
+                else if (AverageGrade >= 80)
+                {
+                    result = "B";
+                }
+                else if (AverageGrade >= 70)
+                {
+                    result = "C";
+                }
+                else if (AverageGrade >= 60)
+                {
+                    result = "D";
+                }
+                else
+                {
+                    result = "F";
+                }
+
+                return result;
+            }
+        }
+        public float AverageGrade;
+        public float HighestGrade;
+        public float LowestGrade;
+    }
+}
